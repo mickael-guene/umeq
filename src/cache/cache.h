2f445702d4376722194b980e91d8d287aabdc00d
Mickael Guene <mickael.guene@st.com>
2014-07-25 10:50:24 +0200

    Add cache library

---
+++ src/cache/cache.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "jitter_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __CACHE__
+#define __CACHE__ 1
+
+#define CACHE_SIZE  (4 * 1024 * 1024)
+
+struct cache {
+    void *(*lookup)(struct cache *cache, uint64_t pc);
+    void (*append)(struct cache *cache, uint64_t pc, void *data, int size);
+};
+
+struct cache *createArmCache(void *memory);
+void cleanCaches(uint64_t from_pc, uint64_t to_pc_exclude);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
