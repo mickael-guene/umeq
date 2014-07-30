ab0055c050e191555c2e436fef5650e321dfe254
Mickael Guene <mickael.guene@st.com>
2014-07-30 13:40:28 +0200

    Add top level testes

---
+++ test/static/nolib/hello/main.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+
+//gcc main.c --static -nostdlib -e main -O0 -m{arm|thumb}
+void hello(int fd, const void *msg, size_t msgSize, int syscallNb)
+{
+    asm volatile(
+        "push {r7}\n\t"
+        "mov r0, %1\n\t"
+        "mov r1, %2\n\t"
+        "mov r2, %3\n\t"
+        "mov r7, %0\n\t"
+        "svc 0\n\t"
+        "pop {r7}\n\t"
+        :
+        : "r" (syscallNb), "r" (fd), "r" (msg), "r" (msgSize)
+        : "r0", "r1", "r2"
+    );
+}
+void bye(int status, int syscallNb)
+{
+    asm volatile(
+        "push {r7}\n\t"
+        "mov r0, %1\n\t"
+        "mov r7, %0\n\t"
+        "svc 0\n\t"
+        "pop {r7}\n\t"
+        :
+        : "r" (syscallNb), "r" (status)
+        : "r0"
+    );
+}
+
+void main()
+{
+    hello(1, "Hello World from arm world\n", sizeof("Hello World from arm world\n"), SYS_write);
+    bye(0, SYS_exit);
+}
+
