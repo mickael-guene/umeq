0a6717b9483d307b00037226cbbe6be1f51ba999
Mickael Guene <mickael.guene@st.com>
2014-09-23 13:57:10 +0200

    Fix strcmp implementation

--- src/runtime/string.c
+++ src/runtime/string.c
@@ -1,9 +1,11 @@
 #include <string.h>
 
+#include <stdio.h>
 int __builtin_strcmp(const char *s1, const char *s2)
 {
-    while ( (*s1++ == *s2++) && (*s1 != '\0') );
-    return (*((unsigned char *)--s1) < *((unsigned char *)--s2)) ? -1 : (*(unsigned char *)s1 != *(unsigned char *)s2);
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(const unsigned char*)s1-*(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
