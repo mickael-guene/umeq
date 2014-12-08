0acbfbe6a18892aca3586f16ee932bb88d8ed911
Mickael Guene <mickael.guene@st.com>
2014-12-08 12:22:40 +0100

    Fix bug in case we call exit from a signal context

--- src/target/arm/arm_syscall.c
+++ src/target/arm/arm_syscall.c
@@ -106,10 +106,16 @@ void arm_hlp_syscall(uint64_t regs)
                 res = arm_mremap(context);
                 break;
             case PR_exit:
-                context->isLooping = 0;
-                context->exitStatus = context->regs.r[0];
-                /* stay in syscall */
-                return ;
+                if (context->is_in_signal) {
+                    /* in case we are in signal handler we leave immediatly */
+                    res = syscall(SYS_exit, context->regs.r[0]);
+                } else {
+                    /* if inside the thread then we will release context memory */
+                    context->isLooping = 0;
+                    context->exitStatus = context->regs.r[0];
+                    /* stay in syscall */
+                    return ;
+                }
                 break;
             default:
                 fatal("You say custom but you don't implement it %d\n", no_neutral);
--- src/target/arm64/arm64_syscall.c
+++ src/target/arm64/arm64_syscall.c
@@ -68,10 +68,16 @@ void arm64_hlp_syscall(uint64_t regs)
                 res = arm64_ptrace(context);
                 break;
             case PR_exit:
-                context->isLooping = 0;
-                context->exitStatus = context->regs.r[0];
-                /* stay in syscall */
-                return ;
+                if (context->is_in_signal) {
+                    /* in case we are in signal handler we leave immediatly */
+                    res = syscall(SYS_exit, context->regs.r[0]);
+                } else {
+                    /* if inside the thread then we will release context memory */
+                    context->isLooping = 0;
+                    context->exitStatus = context->regs.r[0];
+                    /* stay in syscall */
+                    return ;
+                }
                 break;
             default:
                 fatal("You say custom but you don't implement it %d\n", no_neutral);
