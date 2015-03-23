a621d91d82dccb837504ef953af87c416826b90a
Mickael Guene <mickael.guene@st.com>
2015-03-23 09:21:17 +0100

    Fix llseek_s3264 bug

--- src/syscall/syscall32_64/llseek_s3264.c
+++ src/syscall/syscall32_64/llseek_s3264.c
@@ -26,16 +26,16 @@
 
 int llseek_s3264(uint32_t fd_p, uint32_t offset_high_p, uint32_t offset_low_p, uint32_t result_p, uint32_t whence_p)
 {
-    int res;
     int fd = (int) fd_p;
     uint32_t offset_high = offset_high_p;
     uint32_t offset_low = offset_low_p;
     int64_t *result = (int64_t *) g_2_h(result_p);
     unsigned int whence = (unsigned int) whence_p;
     off_t offset = ((long)offset_high << 32) | offset_low;
+    long lseek_res;
 
-    res = syscall(SYS_lseek, fd, offset, whence);
-    *result = res;
+    lseek_res = syscall(SYS_lseek, fd, offset, whence);
+    *result = lseek_res;
 
-    return res<0?res:0;
+    return lseek_res<0?lseek_res:0;
 }
