c9af17e44b0b4535b6a53d57d04c348742ace313
Mickael Guene <mickael.guene@st.com>
2015-03-24 13:31:04 +0100

    Add getpgid arm syscall

--- src/syscall/syscall32_64/syscall32_64.c
+++ src/syscall/syscall32_64/syscall32_64.c
@@ -522,6 +522,9 @@ int syscall32_64(Sysnum no, uint32_t p0, uint32_t p1, uint32_t p2, uint32_t p3, 
         case PR_getitimer:
             res = getitimer_s3264(p0, p1);
             break;
+        case PR_getpgid:
+            res = syscall(SYS_getpgid, (pid_t) p0);
+            break;
         default:
             fatal("syscall_32_to_64: unsupported neutral syscall %d\n", no);
     }
