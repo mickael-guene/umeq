8a58e80fca1783ef485637f17c57d866418ffa9b
Mickael Guene <mickael.guene@st.com>
2015-04-03 09:33:23 +0200

    Add sched_getscheduler arm syscall support

--- src/syscall/syscall32_64/syscall32_64.c
+++ src/syscall/syscall32_64/syscall32_64.c
@@ -558,6 +558,9 @@ int syscall32_64(Sysnum no, uint32_t p0, uint32_t p1, uint32_t p2, uint32_t p3, 
         case PR_sched_getparam:
             res = syscall(SYS_sched_getparam, (pid_t) p0, (struct sched_param *) g_2_h(p1));
             break;
+        case PR_sched_getscheduler:
+            res = syscall(SYS_sched_getscheduler, (pid_t) p0);
+            break;
         default:
             fatal("syscall_32_to_64: unsupported neutral syscall %d\n", no);
     }
