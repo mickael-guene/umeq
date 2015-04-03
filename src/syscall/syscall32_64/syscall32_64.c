f3c897788857d803ff52a9ff1aebd86bdd3aa155
Mickael Guene <mickael.guene@st.com>
2015-04-03 10:24:49 +0200

    Add mq_unlink arm syscall support

--- src/syscall/syscall32_64/syscall32_64.c
+++ src/syscall/syscall32_64/syscall32_64.c
@@ -581,6 +581,9 @@ int syscall32_64(Sysnum no, uint32_t p0, uint32_t p1, uint32_t p2, uint32_t p3, 
         case PR_mq_open:
             res = syscall(SYS_mq_open, (char *) g_2_h(p0), (int) p1, (mode_t) p2, IS_NULL(p3, struct mq_attr));
             break;
+        case PR_mq_unlink:
+            res = syscall(SYS_mq_unlink, (char *) g_2_h(p0));
+            break;
         default:
             fatal("syscall_32_to_64: unsupported neutral syscall %d\n", no);
     }
