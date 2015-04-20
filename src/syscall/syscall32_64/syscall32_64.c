45d58d55b8ea1544a3c8a8f6bbe1c2510b6cfc77
Mickael Guene <mickael.guene@st.com>
2015-04-20 09:35:06 +0200

    add setgid arm syscall support

--- src/syscall/syscall32_64/syscall32_64.c
+++ src/syscall/syscall32_64/syscall32_64.c
@@ -689,6 +689,9 @@ int syscall32_64(Sysnum no, uint32_t p0, uint32_t p1, uint32_t p2, uint32_t p3, 
         case PR_sync:
             res = syscall(SYS_sync);
             break;
+        case PR_setgid:
+            res = syscall(SYS_setgid, (gid_t) p0);
+            break;
         default:
             fatal("syscall_32_to_64: unsupported neutral syscall %d\n", no);
     }
