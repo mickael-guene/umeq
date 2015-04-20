8ed2bb0ea19e0b8ce996e96bf815c33d0d7369cb
Mickael Guene <mickael.guene@st.com>
2015-04-20 08:51:48 +0200

    add setfsuid32 arm syscall support

--- src/syscall/syscall32_64/syscall32_64.c
+++ src/syscall/syscall32_64/syscall32_64.c
@@ -668,6 +668,9 @@ int syscall32_64(Sysnum no, uint32_t p0, uint32_t p1, uint32_t p2, uint32_t p3, 
         case PR_sendfile:
             res = syscall(SYS_sendfile, (int) p0, (int) p1, IS_NULL(p2, off_t), (size_t) p3);
             break;
+        case PR_setfsuid32:
+            res = syscall(SYS_setfsuid, (uid_t) p0);
+            break;
         default:
             fatal("syscall_32_to_64: unsupported neutral syscall %d\n", no);
     }
