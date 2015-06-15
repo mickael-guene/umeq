789c2d69be3cd19ae6b9d83e31ad977b189ecf4f
Mickael Guene <mickael.guene@st.com>
2015-06-15 14:41:05 +0200

    Add arm vneg support

--- src/target/arm/arm_helpers.c
+++ src/target/arm/arm_helpers.c
@@ -3719,6 +3719,44 @@ static void dis_common_vabs_simd(uint64_t _regs, uint32_t insn)
         regs->e.simd[d + r] = res[r];
 }
 
+static void dis_common_vneg_simd(uint64_t _regs, uint32_t insn)
+{
+    struct arm_registers *regs = (struct arm_registers *) _regs;
+    int d = (INSN(22, 22) << 4) | INSN(15, 12);
+    int m = (INSN(5, 5) << 4) | INSN(3, 0);
+    int size = INSN(19, 18);
+    int reg_nb = INSN(6, 6) + 1;
+    int f = INSN(10, 10);
+    int i;
+    int r;
+    union simd_d_register res[2];
+
+    switch(size) {
+        case 0:
+            for(r = 0; r < reg_nb; r++)
+                for(i = 0; i < 8; i++)
+                    res[r].s8[i] = -regs->e.simd[m + r].s8[i];
+            break;
+        case 1:
+            for(r = 0; r < reg_nb; r++)
+                for(i = 0; i < 4; i++)
+                    res[r].s16[i] = -regs->e.simd[m + r].s16[i];
+            break;
+        case 2:
+            for(r = 0; r < reg_nb; r++)
+                for(i = 0; i < 2; i++)
+                    if (f)
+                        res[r].s32[i] = regs->e.simd[m + r].s32[i]^0x80000000;
+                    else
+                        res[r].s32[i] = -regs->e.simd[m + r].s32[i];
+            break;
+        default:
+            fatal("size = %d\n", size);
+    }
+    for(r = 0; r < reg_nb; r++)
+        regs->e.simd[d + r] = res[r];
+}
+
 static void dis_common_vmovn_simd(uint64_t _regs, uint32_t insn)
 {
     struct arm_registers *regs = (struct arm_registers *) _regs;
@@ -4748,6 +4786,9 @@ void hlp_common_adv_simd_two_regs_misc(uint64_t regs, uint32_t insn)
                 break;
             case 12:
                 dis_common_vabs_simd(regs, insn);
+                break;
+            case 14:
+                dis_common_vneg_simd(regs, insn);
                 break;
             default:
                 fatal("a = %d b = 0x%x\n", a, b);
